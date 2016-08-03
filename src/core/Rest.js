36e3d5ddcb3103de9f987e5ba9a57ca10e98a21a
CombatCode <combatcode@gmail.com>
2016-08-03 14:00:48 +0200

    [Dev] Post review changes

--- src/core/Rest.js
+++ src/core/Rest.js
@@ -3,6 +3,10 @@ import restful, { fetchBackend } from 'restful.js';
 
 import { API } from './../settings';
 
+/**
+ * Base class of our API client
+ * @module core/Rest
+ */
 export default class Rest {
     constructor() {
         this.api = restful(API.ADDRESS, fetchBackend(fetch));
