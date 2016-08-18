186d913b31d4b2ea51d37c2be1cbd4f30665353c
Piotr.Gabrielski <piotr.gabrielski@teleste.com>
2016-08-18 14:19:19 +0200

    Fixed rotate condition

--- src/components/Feature/Feature.js
+++ src/components/Feature/Feature.js
@@ -16,6 +16,22 @@ export default class Feature extends L.Marker {
     constructor(latlng, options = {}) {
         super(latlng, options);
     }
+
+    // no alt, ctrl, or shift pressed; not over another selectable marker
+    static rotateCondition(event) {
+        const oEvent = event.originalEvent;
+        if (!oEvent.altKey && !oEvent.ctrlKey && !oEvent.shiftKey) {
+            let targetEl = oEvent.target, mapContainerEl = event.target.getContainer();
+            while (targetEl !== mapContainerEl) {
+                if (L.DomUtil.hasClass(targetEl, 'leaflet-marker-selectable')) {
+                    return false;
+                }
+                targetEl = targetEl.parentNode;
+            }
+            return true
+        }
+        return false;
+    }
 }
 
 /** Inherit options (don't mutate L.Marker defaults) */
@@ -32,21 +48,7 @@ Feature.mergeOptions({
         index: 1
     }],
 
-    // no alt, ctrl, or shift pressed; not over another selectable marker
-    rotateCondition: function(event) {
-        const oEvent = event.originalEvent;
-        if (!oEvent.altKey && !oEvent.shiftKey && !oEvent.shiftKey) {
-            let targetEl = oEvent.target, mapContainerEl = event.target.getContainer();
-            while (targetEl !== mapContainerEl) {
-                if (L.DomUtil.hasClass(targetEl, 'leaflet-marker-selectable')) {
-                    return false;
-                }
-                targetEl = targetEl.parentNode;
-            }
-            return true
-        }
-        return false;
-    }
+    rotateCondition: Feature.rotateCondition
 });
 
 
