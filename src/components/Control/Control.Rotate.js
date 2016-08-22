dd047c7a4f904293ac80260dcb13801730652080
Piotr.Gabrielski <piotr.gabrielski@teleste.com>
2016-08-22 16:37:00 +0200

    Rotate control: change icon Rotate control: removed from Leaflet namespace

--- src/components/Control/Control.Rotate.js
+++ src/components/Control/Control.Rotate.js
@@ -3,19 +3,18 @@ import L from 'leaflet';
 
 /**
  * A one button control to activate rotate handler on selected Markers
- * @class L.Control.Rotate
  * @extends L.Control
  */
-L.Control.Rotate = class Rotate extends L.Control {
+export default class ControlRotate extends L.Control {
     /**
      * Auto. called when L.Control is added to L.Map
      * @param {L.Map} map
      * @returns {Element} Control's DOM element
      */
     onAdd(map) {
-        let container = L.DomUtil.create('div', `${Rotate.className} leaflet-bar`);
+        let container = L.DomUtil.create('div', `${ControlRotate.className} leaflet-bar`);
 
-        this._button = this._createButton(`${Rotate.className}-btn`, {title: this.options.title}, this._onPress,
+        this._button = this._createButton(`${ControlRotate.className}-btn`, {title: this.options.title}, this._onPress,
             this.options.content, container);
 
         L.DomEvent.on(container, 'mouseup mousedown click dblclick', L.DomEvent.stopPropagation);
@@ -142,12 +141,11 @@ L.Control.Rotate = class Rotate extends L.Control {
     }
 };
 
-L.Control.Rotate.className = 'leaflet-control-rotate';
+
+ControlRotate.className = 'leaflet-control-rotate';
 
 
-// @namespace L.Control.Rotate
-// @section Options
-L.Control.Rotate.mergeOptions({
+ControlRotate.mergeOptions({
     position: 'topleft',
     // The text/content of the button.
     // @type {string|Node}
@@ -161,28 +159,3 @@ L.Control.Rotate.mergeOptions({
         return layer instanceof L.Marker && !(layer instanceof L.MarkerCluster)
     }
 });
-
-// @namespace L.Map
-// @section Control options
-L.Map.mergeOptions({
-    // Whether a rotate control is added to the map by default.
-    // @type {boolean}
-    rotateControl: true
-});
-
-L.Map.addInitHook(function() {
-    if (this.options.rotateControl) {
-        this.rotateControl = new L.Control.Rotate();
-        this.addControl(this.rotateControl);
-    }
-});
-
-
-/**
- * Do we actually need this factory? Is it some Leaflet legacy thingy?
- * @namespace Control.Rotate
- * @factory L.control.rotate(options: Control.Rotate options)
- */
-L.control.rotate = function(options) {
-    return new L.Control.Rotate(options);
-};
